e717126e383f90dedeae71a567cc76a4140b8bf2
YamenRM <yamenrafat132@gmail.com>
2026-08-02 23:20:15 +0300

    start

---
+++ src/Configuration.py
@@ -0,0 +1,6 @@
+PROJECT_PATH = "UCL Prediction Tool"
+
+RAW_DOMESTIC_PATH = f"{PROJECT_PATH}/data/raw/matches_domestic.parquet"
+RAW_UCL_PATH = f"{PROJECT_PATH}/data/raw/matches_ucl.parquet"
+PROCESSED_COMBINED_PATH = f"{PROJECT_PATH}/data/processed/matches_combined.parquet"
+FIXTURES_TO_PREDICT_PATH = f"{PROJECT_PATH}/data/processed/fixtures_to_predict.parquet"
