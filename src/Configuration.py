313c48f840279eba37f85143fe8165d7ed78d069
YamenRM <yamenrafat132@gmail.com>
2026-08-04 19:54:59 +0300

    try data scraping

--- src/Configuration.py
+++ src/Configuration.py
@@ -1,6 +1,8 @@
-PROJECT_PATH = "UCL Prediction Tool"
+import os
 
-RAW_DOMESTIC_PATH = f"{PROJECT_PATH}/data/raw/matches_domestic.parquet"
-RAW_UCL_PATH = f"{PROJECT_PATH}/data/raw/matches_ucl.parquet"
-PROCESSED_COMBINED_PATH = f"{PROJECT_PATH}/data/processed/matches_combined.parquet"
-FIXTURES_TO_PREDICT_PATH = f"{PROJECT_PATH}/data/processed/fixtures_to_predict.parquet"
+PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+RAW_DOMESTIC_PATH = f"{PROJECT_PATH}/data/raw/matches_domestic.csv"
+RAW_UCL_PATH = f"{PROJECT_PATH}/data/raw/matches_ucl.csv"
+PROCESSED_COMBINED_PATH = f"{PROJECT_PATH}/data/processed/matches_combined.csv"
+FIXTURES_TO_PREDICT_PATH = f"{PROJECT_PATH}/data/processed/fixtures_to_predict.csv"
